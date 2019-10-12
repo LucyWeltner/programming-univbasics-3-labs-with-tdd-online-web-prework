3f758fd399e3e19149a55f74226e93b7efbcc009
Lucy Weltner <lucyiweltner@gmail.com>
2019-10-12 15:06:41 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -6,7 +6,4 @@ sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
 quotient = first_number/second_number
-mod = first_number%second_number
 exponentiation = first_number ** second_number
-
-puts sum, difference, product, quotient, mod, exponentiation
