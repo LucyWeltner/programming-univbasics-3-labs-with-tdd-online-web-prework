e17a5432e2624dba55e35318465de8e9afbafb1d
Lucy Weltner <lucyiweltner@gmail.com>
2019-10-12 15:04:10 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -7,9 +7,8 @@ difference = first_number - second_number
 product = first_number * second_number
 quotient = first_number/second_number
 mod = first_number%second_number
-logs = first_number log second_number
-
 exponentiation = first_number ** second_number
+
 def add(first_number, second_number)
   sum = first_number + second_number
   puts sum
