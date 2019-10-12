7f52fa284f7d170b984bae21be1d9875eff2ce6a
Lucy Weltner <lucyiweltner@gmail.com>
2019-10-12 15:00:54 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,9 @@
 # Add your variables here
+first_number = 3.0 
+second_number = -5.0 
+def add(first_number, second_number)
+  sum = first_number + second_number
+  puts sum
+end
+
+add(first_number, second_number)
