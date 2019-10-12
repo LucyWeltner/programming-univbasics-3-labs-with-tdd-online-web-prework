26777075b73dfcb62b5bb07499ebd841136ec56c
Lucy Weltner <lucyiweltner@gmail.com>
2019-10-12 15:02:49 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,12 @@
 # Add your variables here
 first_number = 3.0 
 second_number = -5.0 
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number/second_number
+mod = first_number%second_number
+exponentiation = first_number ** second_number
 def add(first_number, second_number)
   sum = first_number + second_number
   puts sum
